422572c4f3d3e53821eef455f4f68b9c6e0357e2
kitaye <44532933+kitaye@users.noreply.github.com>
2021-08-31 00:16:45 +0300

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Привет, я {{text}}</h1>
+</template>
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: ['text'],
+  }
+</script>
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
